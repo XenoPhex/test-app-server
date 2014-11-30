ec4edfabdbd91c5bde890501ae3841e0aaaec9e0
Anand Gaitonde <agaitonde@pivotal.io>
2014-11-30 04:37:38 -0500

    Remote Sleep that hits Sleeper-API.

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'newrelic_rpm'
+require 'curl'
+require 'json'
 
 get '/' do
     "Welcome to the test app!"
@@ -8,4 +10,14 @@ end
 get '/env' do
   headers 'Content-Type' => 'text/plain'
   "ENV Vars:\n#{`env`.chomp}"
+end
+
+get '/remote_wait/:sleep_time' do |sleep_time|
+  result = JSON::parse(Curl.get("http://sleeper-api.cfapps.io/#{sleep_time}").body_str)
+
+  headers 'Content-Type' => 'text/json'
+  {
+    remote_wait: sleep_time,
+    success: result['sleep_time'] && result['sleep_time'].to_s == sleep_time
+  }.to_json
 end
